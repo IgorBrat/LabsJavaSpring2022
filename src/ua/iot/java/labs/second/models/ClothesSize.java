67750db41400ad5dc596f40b460c3d4a44b21d1c
IgorBrat <akbars5678@gmail.com>
2022-04-07 16:39:57 +0300

    Added models(classes and enums for them)

---
+++ src/ua/iot/java/labs/second/models/ClothesSize.java
@@ -0,0 +1,5 @@
+package ua.iot.java.labs.second.models;
+
+public enum ClothesSize {
+	XXS, XS, S, M, L, XL, XXL
+}
---
+++ src/ua/iot/java/labs/second/models/Gender.java
@@ -0,0 +1,5 @@
+package ua.iot.java.labs.second.models;
+
+public enum Gender {
+	MALE, FEMALE, INTERSEX
+}
