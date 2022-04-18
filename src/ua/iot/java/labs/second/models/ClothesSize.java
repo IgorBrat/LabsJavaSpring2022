10683bc4695596a9a29f5f713b4a27925ca0b93d
IgorBrat <akbars5678@gmail.com>
2022-04-18 16:43:31 +0300

    Deleted unnecessary directory

--- src/ua/iot/java/labs/second/models/ClothesSize.java
+++
@@ -1,5 +0,0 @@
-package ua.iot.java.labs.second.models;
-
-public enum ClothesSize {
-	XXS, XS, S, M, L, XL, XXL
-}
--- src/ua/iot/java/labs/second/models/Gender.java
+++
@@ -1,5 +0,0 @@
-package ua.iot.java.labs.second.models;
-
-public enum Gender {
-	MALE, FEMALE, INTERSEX
-}
